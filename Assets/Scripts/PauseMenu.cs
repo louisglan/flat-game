8b2cc82d184bfcb2df1965501fcced67be3c6ee1
Louis <louissam@hotmail.co.uk>
2025-04-16 19:14:20 +0100

    added pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static bool IsPaused = false;
+    public GameObject pauseMenuUI;
+
     void Start()
     {
-        
+        Resume();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnEscapeKeyPressed();
+        }
+    }
+
+    private void OnEscapeKeyPressed()
+    {
+        if (IsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        IsPaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Pause()
+    {
+        IsPaused = true;
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Loading Screen");
     }
 }
